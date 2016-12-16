0aadb219926d71bf1de3a94d1fc735cc64a63ea5
Okazari <plouzennec.benjamin@gmail.com>
2016-12-16 15:10:39 +0100

    improved ball animation start

--- movezen.js
+++ movezen.js
@@ -26,6 +26,7 @@ const backgroundCircle = new mojs.Shape({
   duration: 1000,
   onComplete () {
     tree.play()
+    ball.play()
   },
 }).play()
 
@@ -174,17 +175,14 @@ const z = new mojs.Shape({
   duration: letterDuration,
 }).then(letterStep2)
 
-var bouncyEasing = mojs.easing.path('M0,100 C6.50461245,96.8525391 12.6278439,88.3497543 16.6678547,0 C16.6678547,-1.79459817 31.6478577,115.871587 44.1008572,0 C44.1008572,-0.762447191 54.8688736,57.613472 63.0182497,0 C63.0182497,-0.96434046 70.1500549,29.0348701 76.4643231,0 C76.4643231,0 81.9085007,16.5050125 85.8902733,0 C85.8902733,-0.762447191 89.4362183,8.93311024 92.132216,0 C92.132216,-0.156767385 95.0157166,4.59766248 96.918051,0 C96.918051,-0.156767385 98.7040751,1.93815588 100,0');
-
 const ball = new mojs.Shape({
   shape: 'circle',
   fill: '#b31835',
   radius: 20,
-  isShowStart: true,
   duration: 1500,
   x: -70,
   onUpdate: function (progress) {
     let bounceProgress = mojs.easing.bounce.out(progress);
-    ball.tune({ y: bounceProgress*200 - 200})
+    ball.tune({ y: bounceProgress*200 - 100})
   }
-}).play()
+})
