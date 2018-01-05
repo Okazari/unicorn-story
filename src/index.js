27d03527ce0075084d72a94a4c9b2b71f2f6bddf
Delphine <delphine.millet@zenika.com>
2018-01-05 13:21:38 +0100

    :construction: add unicorn shapes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { chain, tween, styler, timeline } from 'popmotion'
+import { chain, tween, styler, timeline, easing } from 'popmotion'
 
 /*** BACKGROUND ***/
 const backgroundCircle = document.getElementById('backgroundCircle')
 const backgroundCircleStyler = styler(backgroundCircle)
 
-const drawCircle = tween({
+tween({
   from: { r: 0 },
-  to: { r: 50 },
+  to: { r: 100 },
   duration: 1000,
 })
 .start(backgroundCircleStyler.set)
@@ -16,12 +16,9 @@ const body = document.getElementById('body');
 const bodyStyler = styler(body);
 const bodyLength = body.getTotalLength()
 
-bodyStyler.set('stroke', 'white')
 bodyStyler.set('stroke-dasharray', bodyLength)
-bodyStyler.set('fill', '#FFF')
-bodyStyler.set('fillOpacity', 0)
 
-const drawBody = tween({
+tween({
   from: { strokeDashoffset: -bodyLength },
   to: { strokeDashoffset: 0 },
   duration: 2000,
@@ -40,6 +37,45 @@ chain(
   })
 )
 .start(bodyStyler.set);
+
+/*** HAIR ***/
+const hair = document.getElementById('hair');
+const hairStyler = styler(hair);
+
+tween({
+  from: { y: -800 },
+  to: { y: 0 },
+  duration: 1000,
+  easing: easing.backOut,
+})
+.start(hairStyler.set)
+
+/*** TAIL ***/
+const tail = document.getElementById('tail');
+const tailStyler = styler(tail);
+
+tween({
+  from: { x: -800 },
+  to: { x: 0 },
+  duration: 1000,
+  easing: easing.backOut,
+})
+.start(tailStyler.set)
+
+/*** EYE ***/
+const eye = document.getElementById('eye');
+const eyeStyler = styler(eye);
+
+/*** HORN ***/
+const horn = document.getElementById('horn');
+const hornStyler = styler(horn);
+
+tween({
+  from: { x: -40, y: 30 },
+  to: { x: 0, y: 0 },
+  duration: 1500,
+})
+.start(hornStyler.set)
 
 // timeline([
 //   { track: 'backgroundCircle', from: { r: 0 }, to: { r: 50 }, duration: 1000 },
