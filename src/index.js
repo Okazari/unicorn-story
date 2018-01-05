a3b81eb8273e15300fc931326ea8faa8576dd3d3
Delphine <delphine.millet@zenika.com>
2018-01-05 12:06:40 +0100

    :heavy_plus_sign: add popmotion

--- src/index.js
+++ src/index.js
@@ -1,38 +1,53 @@
-import {
-  backgroundCircle,
-  horn,
-  body,
-  hair,
-  tail,
-  eye,
-  burst,
-} from './Parts'
+import { chain, tween, styler, timeline } from 'popmotion'
 
-backgroundCircle.then({
-  duration: 0,
-  onComplete() {
-    body.play()
-  }
-}).play()
+/*** BACKGROUND ***/
+const backgroundCircle = document.getElementById('backgroundCircle')
+const backgroundCircleStyler = styler(backgroundCircle)
 
-body.then({
-  duration: 0,
-  onComplete() {
-    horse.play()
-  }
+const drawCircle = tween({
+  from: { r: 0 },
+  to: { r: 50 },
+  duration: 1000,
+})
+.start(backgroundCircleStyler.set)
+
+/*** BODY ***/
+const body = document.getElementById('body');
+const bodyStyler = styler(body);
+const bodyLength = body.getTotalLength()
+
+bodyStyler.set('stroke', 'white')
+bodyStyler.set('stroke-dasharray', bodyLength)
+bodyStyler.set('fill', '#FFF')
+bodyStyler.set('fillOpacity', 0)
+
+const drawBody = tween({
+  from: { strokeDashoffset: -bodyLength },
+  to: { strokeDashoffset: 0 },
+  duration: 2000,
 })
 
-const horse = new mojs.Timeline({
-  onComplete() {
-    horn.play()
-  }
-})
-.add(hair)
-.add(tail)
-.add(eye)
+chain(
+  tween({
+    from: { strokeDashoffset: -bodyLength },
+    to: { strokeDashoffset: 0 },
+    duration: 2000,
+  }),
+  tween({
+    from: { fillOpacity: 0 },
+    to: { fillOpacity: 1 },
+    duration: 1000,
+  })
+)
+.start(bodyStyler.set);
 
-document.addEventListener( 'click', function (e) {
-  burst
-    .tune({ x: e.pageX, y: e.pageY })
-    .replay();
-} );
+// timeline([
+//   { track: 'backgroundCircle', from: { r: 0 }, to: { r: 50 }, duration: 1000 },
+//   { track: 'body', from: { strokeDashoffset: -bodyLength }, to: { strokeDashoffset: 0 }, duration: 2000 },
+//   [
+//     { track: 'body', from: { fillOpacity: 0 }, to: { fillOpacity: 1 }, duration: 1000 }
+//   ]
+// ]).start(({backgroundCircle, body}) => {
+//   backgroundCircleStyler.set(backgroundCircle);
+//   bodyStyler.set(body)
+// })
