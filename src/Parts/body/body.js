8484551c9a60eb1b331d17926143442160e5dda1
Plouzennec Benjamin <plouzennec.benjamin@gmail.com>
2018-01-10 13:08:39 +0100

    :lipstick: Resized body stroke width

--- src/Parts/body/body.js
+++ src/Parts/body/body.js
@@ -1,13 +1,14 @@
-import { chain, tween, styler, action } from 'popmotion'
+import { chain, tween, styler, action, easing } from 'popmotion'
 
 const bodyPath = document.getElementById('body');
 const bodyStyler = styler(bodyPath);
 bodyStyler.set('opacity', 0)
-
+bodyStyler.set('strokeWidth', 0.5)
 const drawBody = tween({
   from: { pathLength: 0, opacity: 1 },
   to: { pathLength: 100 },
   duration: 2000,
+  ease: easing.easeInOut
 })
 
 const fillBody = tween({
